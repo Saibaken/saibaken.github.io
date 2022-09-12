9a6739bbe520ccc313a216e4e0c0726173004e18
Saibaken <shelamov.s@gmail.com>
2022-09-12 20:38:54 +0300

    feat: :sparkles: Написал кнопку для возврата на домашнюю страницу

--- src/components/BackButton/BackButton.tsx
+++ src/components/BackButton/BackButton.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './BackButton.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
-type Props = {}
-
-export default function BackButton({ }: Props) {
+export default function BackButton() {
     return (
-        <Link to='/' className=''>←</Link>
+         <Link to='/' className='back-button'>
+             <FontAwesomeIcon icon={faArrowLeft}/>
+         </Link>
+
     )
 }
